87364dd502a0cbca4bcc6547ce810143bfa7d25b
Kvas <shokhin.al@gmail.com>
2021-10-19 13:29:13 +0300

    Task 3. Setup picocli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "gendiff", version = "Differ 1.0", mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+    // @CommandLine.Option(names = {"-u", "--upper"}, description = "Upper Case")
+    // boolean isUpperCase = false;
+    //
+    // @CommandLine.Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
+    //         description = "Word to be printed.")
+    // String text = "Hello picocli"; // defaultValue in @Parameters will be used if no given in args
+
+    @Override
+    public void run() {
+
+        String diff = Differ.generate("hello", "world");
+        System.out.println(diff);
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,10 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+
+public class Differ {
+
+    public static String generate(String fp1, String fp2) {
+        return "no";
+    }
+}
